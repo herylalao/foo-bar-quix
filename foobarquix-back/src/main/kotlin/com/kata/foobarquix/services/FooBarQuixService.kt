dc6a6ba8bdce376eb1ae09ab45260866c8eb245e
herylalao <dagocoop@yahoo.fr>
2021-02-01 00:22:28 +0100

    implementation of foo-bar-quix
    Tests and implementation of foo-bar-quix

--- foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
+++ foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
@@ -6,7 +6,28 @@ import org.springframework.stereotype.Component
 class FooBarQuixService {
 
     fun convertNumber(inputNumber: Int): String {
-        return ""
+        var retour:String
+        retour = ""
+        if (inputNumber % 3 == 0)
+        {
+            retour = retour + "Foo"
+        }
+        if (inputNumber % 5 == 0)
+        {
+            retour = retour + "Bar"
+        }
+        val chaine = (inputNumber).toString()
+        for (i in 0 until chaine.length)
+        {
+            val c = chaine.get(i)
+            when (c) {
+                '3' -> retour = retour + "Foo"
+                '5' -> retour = retour + "Bar"
+                '7' -> retour = retour + "Quix"
+                else -> {}
+            }
+        }
+        return if (retour.isEmpty()) chaine else retour
     }
 
 }
--- foobarquix-back/src/test/kotlin/com/kata/foobarquix/FooBarQuixApplicationTests.kt
+++ foobarquix-back/src/test/kotlin/com/kata/foobarquix/FooBarQuixApplicationTests.kt
@@ -2,12 +2,73 @@ package com.kata.foobarquix
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.api.Assertions.*
+import com.kata.foobarquix.services.FooBarQuixService
 
 @SpringBootTest
 class FooBarQuixApplicationTests {
 
+	private val objService = FooBarQuixService()
+
+
 	@Test
-	fun contextLoads() {
+	fun test_FBQ_1_E_1() {
+		val retour = objService.convertNumber(1)
+		assertEquals("1", retour, "fbq(1)=1")
+	}
+
+	@Test
+	fun test_FBQ_3_E_FooFoo() {
+		val retour = objService.convertNumber(3)
+		assertEquals("FooFoo", retour, "fbq(3)=FooFoo")
+	}
+
+	@Test
+	fun test_FBQ_5_E_BarBar() {
+		val retour = objService.convertNumber(5)
+		assertEquals("BarBar", retour, "fbq(5)=BarBar")
+	}
+
+	@Test
+	fun test_FBQ_7_E_Quix() {
+		val retour = objService.convertNumber(7)
+		assertEquals("Quix", retour, "fbq(7)=Quix")
+	}
+
+	@Test
+	fun test_FBQ_9_E_Foo() {
+		val retour = objService.convertNumber(9)
+		assertEquals("Foo", retour, "fbq(9)=Foo")
+	}
+
+	@Test
+	fun test_FBQ_51_E_FooBar() {
+		val retour = objService.convertNumber(51)
+		assertEquals("FooBar", retour, "fbq(51)=FooBar")
+	}
+
+	@Test
+	fun test_FBQ_53_E_BarFoo() {
+		val retour = objService.convertNumber(53)
+		assertEquals("BarFoo", retour, "fbq(53)=BarFoo")
+	}
+
+	@Test
+	fun test_FBQ_33_E_FooFooFoo() {
+		val retour = objService.convertNumber(33)
+		assertEquals("FooFooFoo", retour, "fbq(33)=FooFooFoo")
+	}
+
+	@Test
+	fun test_FBQ_27_E_FooQuix() {
+		val retour = objService.convertNumber(27)
+		assertEquals("FooQuix", retour, "fbq(27)=FooQuix")
+	}
+
+	@Test
+	fun test_FBQ_15_E_FooBarBar() {
+		val retour = objService.convertNumber(15)
+		assertEquals("FooBarBar", retour, "fbq(15)=FooBarBar")
 	}
 
 }
