45aa9ecbcb59619468555bb5236ebdf185fb27cb
herylalao <dagocoop@yahoo.fr>
2021-02-01 00:32:35 +0100

    Changes to foo-bar-quix.service.ts
    Changes to foo-bar-quix.service.ts to call the backend service

--- foobarquix-ui/src/app/foo-bar-quix.service.ts
+++ foobarquix-ui/src/app/foo-bar-quix.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable, Injector } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class FooBarQuixService {
-  constructor() { }
+  constructor(private http: HttpClient, private injector: Injector) { }
+  
+  public convertNumber(inputNumber: Number):Observable<object> {
+    const serveur = this.injector.get('SERVER_URL');
+    console.log("serveur:" + serveur);
+    return this.http.get<object>(serveur+'/foo-bar-quix/'+ inputNumber);
+  }
 
 }
